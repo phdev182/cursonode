6034164ca64f58c34a32b64fcd7f4dbbc3a7a7cb
“phdev” <phdev182@gmail.com>
2021-03-06 11:13:21 -0400

    Post, put and Delete

--- cursonode1/src/index.js
+++ cursonode1/src/index.js
@@ -4,9 +4,36 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-  return response.json({
-    message: 'Hello Dev' });
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
 });
+
+app.post('/projects', (req, response) => {
+  return response.json([
+  'Projeto 1',
+  'Projeto 2',
+  'Projeto 3',
+  'Projeto 4'
+  ]);
+});
+
+app.put('/projects', (req, response) => {
+  return response.json([
+  'Projeto 1',
+  'Projeto 2',
+  'Projeto 3',
+  'Projeto 4'
+  ])
+})
+
+app.delete('/projects', (req, response) => {
+  return response.json([
+    'Projeto 3'
+  ])
+})
 
 app.listen(3333, () =>
     console.log('backend started! 🤪 ')
